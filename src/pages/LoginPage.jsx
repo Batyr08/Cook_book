1e0c6e9053df6fbc9fb7e8ff348c12b5ddf09aca
frekler <frekler@gmail.com>
2024-01-17 01:19:02 +0100

    login/signup init

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,39 @@
 import React from 'react';
 
 export default function LoginPage() {
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append('email', e.target.email.value);
+    data.append('password', e.target.password.value);
+    const res = Object.fromEntries(data);
+    if (!res.email || !res.password) {
+      alert('ZAPOLNI');
+    } else {
+      try {
+        const response = await fetch('/api/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(res),
+        });
+        console.log('response:', response);
+        const result = await response.json();
+        console.log('result:', result);
+        if (result.msg) {
+          window.location.href = '/';
+        }
+        if (result.err) {
+          console.log(result.err);
+        }
+      } catch (error) {
+        console.log('Ошибка авторизации', error);
+        alert('Ошибка авторизации');
+      }
+    }
+  };
+
   return (
     <>
       <link rel="stylesheet" href="/css/logregform.css" />
@@ -8,7 +41,7 @@ export default function LoginPage() {
       <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
       <div className="center">
         <h1> Войдите в аккаунт </h1>
-        <form>
+        <form onSubmit={handlerSubmit}>
           <div className="txt_field">
             <input name="email" type="text" required />
             <label>Email</label>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,6 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SignUpPage() {
+  // const [formData, setFormData] = useState({
+  //   email: "",
+  //   login: "",
+  //   password: "",
+  //   repeat: ""
+  //  })
+  //  const changeHandler = (event) => setFormData((prev) => ({ ...prev, [event.target.name] : event.target.value }) );
+  //  const handlerSubmit = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     const response
+  //   } catch (error) {
+
+  //   }
+  //  }
+
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append('email', e.target.email.value);
+    data.append('login', e.target.login.value);
+    data.append('password', e.target.password.value);
+    data.append('repeat', e.target.repeat.value);
+    const res = Object.fromEntries(data);
+    if (!res.email || !res.login || !res.password) {
+      alert('ZAPOLNI');
+    } else {
+      try {
+        const response = await fetch('/api/auth/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(res),
+        });
+        console.log('response:', response);
+        const result = await response.json();
+        console.log('result:', result);
+        if (result.msg) {
+          window.location.href = '/';
+        }
+        if (result.err) {
+          console.log(result.err);
+        }
+      } catch (error) {
+        console.log('Ошибка регистарции', error);
+        alert('Ошибка регистрации');
+      }
+    }
+  };
+
   return (
     <>
       <link rel="stylesheet" href="/css/logregform.css" />
@@ -8,14 +59,16 @@ export default function SignUpPage() {
       <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
       <div className="center">
         <h1>Зарегистрируйтесь</h1>
-        <form>
+        <form onSubmit={handlerSubmit}>
+          {' '}
+          {/* action='/api/auth/signup' method='POST' onSubmit={handlerSubmit} */}
           <div className="txt_field">
             <input name="email" type="email" required />
             <label>Email</label>
             <span />
           </div>
           <div className="txt_field">
-            <input name="name" type="text" id="inputname" required />
+            <input name="login" type="text" id="inputname" required />
             <label>Имя</label>
             <span />
           </div>
